9eb605716cfde28f334504965cb93b827c1b48c9
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2022-05-24 11:31:06 -0500

    update welcome section content

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,103 +1,49 @@
 (function () {
-    Zfont.init(Zdog);
-
-    // Create Zdog Illustration
-    // https://zzz.dog/api#illustration
-    var illo = new Zdog.Illustration({
-        element: ".zdog-canvas",
-        dragRotate: true,
-        resize: "fullscreen",
-    });
-
-    // Create a Font object
-    // You can use any .ttf or .otf font!
-    // https://github.com/jaames/zfont#zdogfont
-    var font = new Zdog.Font({
-        src: "https://homiemace.github.io/css/fonts/bold.otf",
-    });
-
-    // Create a TextGroup object for the title
-    // https://github.com/jaames/zfont#zdogtextgroup
-    var title = new Zdog.TextGroup({
-        addTo: illo,
-        font: font,
-        value: "Homie",
-        fontSize: 60,
-        translate: { y: -100 },
-        textAlign: "center",
-        textBaseline: "middle",
-        color: "#6667AB",
-        fill: true,
-    });
-    illo.updateRenderGraph();
-})();
-
-(function () {
-  const Welcome_Container = documnent.querySelector(".welcome");
-  const Logo_Container = Welcome_Container.querySelector(".logo");
-  const Logo_Text_Container = Logo_Container.querySelector(".logo-text-container");
-  const Logo_Text = Logo_Text_Container.querySelector(".logo-text");
-  Logo_Text.innerHTML = Logo_Text.textContent.replace(/\S/g,`<span class="letter">$&</span>`)
-  const Slogan_Container = Logo_Container.querySelector(".slogan-container");
-  const Slogan_Text = Slogan_Container.querySelector(".slogan-text");
-  Slogan_Text.innerHTML = Slogan_Text.textContent.replace(/\S/g,`<span class="letter">$&</span>`)
-
-  var ml4 = {};
-    ml4.opacityIn = [0, 1];
-    ml4.scaleIn = [0.2, 1];
-    ml4.scaleOut = 3;
-    ml4.durationIn = 800;
-    ml4.durationOut = 600;
-    ml4.delay = 500;
+    const Welcome_Container = document.querySelector(".welcome");
+    const Logo_Container = Welcome_Container.querySelector(".logo");
+    const Logo_Text_Container = Logo_Container.querySelector(".logo-text-container");
+    const Logo_Text = Logo_Text_Container.querySelector(".logo-text");
+    Logo_Text.innerHTML = Logo_Text.textContent.replace(/\S/g, `<span class="letter">$&</span>`);
+    const Logo_Text_Letters = Logo_Text.querySelectorAll(".letter");
+    const Slogan_Container = Logo_Container.querySelector(".slogan-container");
+    const Slogan_Text = Slogan_Container.querySelector(".slogan-text");
+    Slogan_Text.innerHTML = Slogan_Text.textContent.replace(/\S/g, `<span class="letter">$&</span>`);
+    const Slogan_Text_Letters = Slogan_Text.querySelectorAll(".letter");
 
     anime
         .timeline({ loop: false })
         .add({
-            targets: ".ml4 .letters-1",
-            opacity: ml4.opacityIn,
-            scale: ml4.scaleIn,
-            duration: ml4.durationIn,
+            targets: Logo_Text_Letters,
+            opacity: [0, 1],
+            scale: [0.2, 1],
+            duration: 1000,
+            delay: anime.stagger(50),
+            begin: () => {
+                Logo_Text.style["display"] = "block";
+                Slogan_Text.style["display"] = "block";
+            },
         })
         .add({
-            targets: ".ml4 .letters-1",
-            opacity: 0,
-            scale: ml4.scaleOut,
-            duration: ml4.durationOut,
-            easing: "easeInExpo",
-            delay: ml4.delay,
-        })
-        .add({
-            targets: ".ml4 .letters-2",
-            opacity: ml4.opacityIn,
-            scale: ml4.scaleIn,
-            duration: ml4.durationIn,
-        })
-        .add({
-            targets: ".ml4 .letters-2",
-            opacity: 0,
-            scale: ml4.scaleOut,
-            duration: ml4.durationOut,
-            easing: "easeInExpo",
-            delay: ml4.delay,
-        })
-        .add({
-            targets: ".ml4 .letters-3",
-            opacity: ml4.opacityIn,
-            scale: ml4.scaleIn,
-            duration: ml4.durationIn,
-        })
-        .add({
-            targets: ".ml4 .letters-3",
-            opacity: 0,
-            scale: ml4.scaleOut,
-            duration: ml4.durationOut,
-            easing: "easeInExpo",
-            delay: ml4.delay,
-        })
-        .add({
-            targets: ".ml4",
-            opacity: 0,
-            duration: 500,
-            delay: 500,
+            targets: Slogan_Text_Letters,
+            opacity: [0, 1],
+            scale: [0.2, 1],
+            duration: 1000,
+            delay: anime.stagger(50),
         });
+})();
+
+(function () {
+    const Site_Banner = document.querySelector(".page-section.welcome .site-banner");
+    const Bottom_Layer = Site_Banner.querySelector(".bottom-layer");
+
+    function Update() {
+        Site_Banner.style["width"] = `${window.innerWidth}px`;
+        Site_Banner.style["height"] = `${window.innerHeight}px`;
+        Site_Banner.setAttribute("viewBox", `0 0 ${window.innerWidth / 2} ${window.innerHeight / 2}`);
+        Bottom_Layer.setAttribute("transform", `translate(${window.innerWidth / 2}, ${window.innerHeight / 2})`);
+    }
+
+    Update();
+
+    window.addEventListener("resize", Update);
 })();
